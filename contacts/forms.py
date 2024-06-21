22de9035c10d0af03de8bce456e8da82cbc82c7d
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-06-20 22:03:11 -0300

    Set length to contact phone field

--- contacts/forms.py
+++ contacts/forms.py
@@ -11,28 +11,28 @@ class CreateContactForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['description'].widget.attrs.update({
-            'container_class': 'description-input'
-        })
+        self.fields["description"].widget.attrs.update(
+            {"container_class": "description-input"}
+        )
 
-        self.fields['image'].widget.attrs.update({
-            'container_class': 'image-input'
-        })
+        self.fields["image"].widget.attrs.update({"container_class": "image-input"})
 
         for field_name in self.fields:
-            self.fields[field_name].widget.attrs.update(
-                {'class': 'field-input'})
+            self.fields[field_name].widget.attrs.update({"class": "field-input"})
+
+    phone = forms.CharField(
+        min_length=11,
+        max_length=11,
+    )
 
     class Meta:
         model = Contact
-        fields = ["name", 'surname', 'phone', 'email', 'description', 'image']
+        fields = ["name", "surname", "phone", "email", "description", "image"]
 
     def clean_phone(self):
-        phone = self.cleaned_data.get('phone')
+        phone = self.cleaned_data.get("phone")
 
-        if re.search(r'[^0-9]', phone):
-            raise ValidationError(
-                'The phone field must contain only numbers'
-            )
+        if re.search(r"[^0-9]", phone):
+            raise ValidationError("The phone field must contain only numbers")
 
         return phone
