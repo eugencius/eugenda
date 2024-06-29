e55c9deb0810946ecd920f1a4aa560c5709e8357
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-06-29 12:12:16 -0300

    Implement pagination for contacts

--- contacts/views.py
+++ contacts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 
 from . import forms
 from .models import Contact
+from templates.static import utils
 
 
 User = get_user_model()
@@ -14,13 +16,23 @@ User = get_user_model()
 @login_required(login_url="accounts:login")
 def index_view(request):
     user = User.objects.get(username=request.user.username)
-    contacts = Contact.objects.filter(creator=user)
+
+    per_page = 9
+    contacts = Contact.objects.filter(creator=user).order_by("-id")
+    paginator = Paginator(contacts, 9)
+
+    current_page = int(request.GET.get("page", 1))
+    page_obj = paginator.get_page(current_page)
+    page_range = utils.make_pagination(current_page, paginator.page_range, max_pages=6)
 
     return render(
         request,
         "contacts/index.html",
         {
-            "contacts": contacts,
+            "page_range": page_range,
+            "page_obj": page_obj,
+            "current_page": current_page,
+            "is_paginated": paginator.count > per_page,
         },
     )
 
--- templates/static/utils.py
+++ templates/static/utils.py
@@ -1,2 +1,20 @@
 def set_placeholder(field, placeholder):
-    field.widget.attrs['placeholder'] = placeholder
+    field.widget.attrs["placeholder"] = placeholder
+
+
+def make_pagination(current_page, page_range, max_pages):
+    mid_page = max_pages // 2
+    start_page = (current_page - mid_page) - 1
+    end_page = current_page + mid_page
+    total_pages = len(page_range)
+
+    start_page_offset = abs(start_page) if start_page < 0 else 0
+
+    if start_page < 0:
+        start_page = 0
+        end_page += start_page_offset
+
+    if end_page >= total_pages:
+        start_page = start_page - abs(total_pages - end_page)
+
+    return page_range[start_page:end_page]
