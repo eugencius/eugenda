051c1cc3ac8d3585859915d6271b11bdb5be7288
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-08-01 22:37:36 -0300

    Implement CRUD in API

--- contacts/views/api.py
+++ contacts/views/api.py
@@ -8,24 +8,67 @@ from ..serializers import ContactSerializer
 
 @api_view(http_method_names=["POST"])
 def contacts_api_create(request):
-    pass
+    user = request.user
+    category_id = request.data.get("category_id")
+
+    serializer = ContactSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save(creator=user, category_id=category_id)
+
+    return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
 
 
 @api_view(http_method_names=["GET"])
 def contacts_api_list(request):
-    pass
+    user = request.user
+
+    contacts = (
+        Contact.objects.filter(creator=user).order_by("-id").select_related("category")
+    )
+    serializer = ContactSerializer(instance=contacts, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(http_method_names=["PATCH"])
 def contacts_api_edit(request, pk):
-    pass
+    user = request.user
+
+    contact = Contact.objects.filter(creator=user, pk=pk).first()
+    if not contact:
+        return Response({"detail": "Eita!"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ContactSerializer(
+        instance=contact,
+        data=request.data,
+        many=False,
+        partial=True,
+    )
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(http_method_names=["DELETE"])
 def contacts_api_delete(request, pk):
-    pass
+    user = request.user
+    contact = Contact.objects.filter(creator=user, pk=pk).first()
+
+    if not contact:
+        return Response({"detail": "Eita!"}, status=status.HTTP_404_NOT_FOUND)
+
+    contact.delete()
+    return Response(status=status.HTTP_200_OK)
 
 
 @api_view()
 def contacts_api_details(request, pk):
-    pass
+    user = request.user
+
+    contact = Contact.objects.filter(creator=user, pk=pk).first()
+    if not contact:
+        return Response({"detail": "Eita!"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ContactSerializer(instance=contact, many=False)
+    return Response(serializer.data, status=status.HTTP_200_OK)
