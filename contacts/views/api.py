f86e62513748396e4f42302dcb81e301172a0ca6
Vinícius Eugênio <viniciuseugeniovhe@gmail.com>
2024-08-02 18:14:50 -0300

    Use generic views in API view classes instead of APIView

--- contacts/views/api.py
+++ contacts/views/api.py
@@ -1,69 +1,45 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-from ..models import Contact, Category
+from ..models import Contact
 from ..serializers import ContactSerializer
 
 
-class ContactsListAPI(APIView):
-    def get(self, request):
+class FilterQuerysetUser:
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        user = self.request.user
+        qs = qs.filter(creator=user)
+
+        return qs
+
+
+class ContactsListAPI(FilterQuerysetUser, ListCreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset().order_by("-id")
+        return qs
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
         user = request.user
 
-        contacts = (
-            Contact.objects.filter(creator=user)
-            .order_by("-id")
-            .select_related("category")
-        )
-        serializer = ContactSerializer(instance=contacts, many=True)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        user = request.user
-        category_id = request.data.get("category_id")
-
-        serializer = ContactSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save(creator=user, category_id=category_id)
-
-        return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-
-
-class ContactsDetailsAPI(APIView):
-    def get_contact(self, user, pk):
-        contact = Contact.objects.filter(creator=user, pk=pk).first()
-
-        if not contact:
-            raise NotFound()
-
-        return contact
-
-    def get(self, request, pk):
-        user = request.user
-        contact = self.get_contact(user, pk)
-
-        serializer = ContactSerializer(instance=contact, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def patch(self, request, pk):
-        user = request.user
-        contact = self.get_contact(user, pk)
-
-        serializer = ContactSerializer(
-            instance=contact,
+        contact = ContactSerializer(
             data=request.data,
-            many=False,
             partial=True,
         )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        contact.is_valid(raise_exception=True)
+        contact.save(creator=user)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(contact.data, status=status.HTTP_201_CREATED)
 
-    def delete(self, request, pk):
-        user = request.user
-        self.get_contact(user, pk).delete()
-        return Response(status=status.HTTP_200_OK)
+
+class ContactsDetailsAPI(FilterQuerysetUser, RetrieveUpdateDestroyAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
